5b3771eafe65d7ccb2e0303d71a4c6cb794c121a
gabriellaxavier05 <gabriellaxaviersp05@outlook.com>
2024-12-26 18:28:15 -0300

    Exercício sobre pacotes externos (npm) do curso

---
+++ Mod05/Aprendendo npm/index.js
@@ -0,0 +1,4 @@
+const generateName = require('sillyname') // importação do módulo sillyname e atribuição à variável generateName
+const sillyname = generateName() // vai ser tipo um método/função
+
+console.log(`Meu nome é ${sillyname}`)
